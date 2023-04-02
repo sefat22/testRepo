6bebf9451f7b46e001c5896210b201219ae30894
sefat22 <sefate1599@gmail.com>
2023-04-02 17:37:10 -0400

    added new text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Test Project");
     }
 }
+
+
+/*
+
+initially:
+    git init
+    git commit -m 'first message'
+    git branch -m main
+    git remote..
+    git push
+
+incremental updates:
+    git add .
+    git commit -m 'message'
+    git push
+
+ */
